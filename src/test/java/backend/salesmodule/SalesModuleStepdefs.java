a547b4ee00cb69d7d1cf4c9104cfe3d0db16990b
YALKUNT <141465745+YALKUNT@users.noreply.github.com>
2023-12-10 12:09:13 +0100

    12/12/2023

--- src/test/java/backend/salesmodule/SalesModuleStepdefs.java
+++ src/test/java/backend/salesmodule/SalesModuleStepdefs.java
@@ -5,16 +5,44 @@ import basefunc.LoginDataForBackEnd;
 import dashboard.DashBoardPageForBackEnd;
 import dashboard.LoginPageForBackEnd;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class SalesModuleStepdefs extends BaseClassForBackend {
     LoginDataForBackEnd loginDataForBackEnd = new LoginDataForBackEnd();
     LoginPageForBackEnd loginPageForBackEnd = new LoginPageForBackEnd(driver);
     DashBoardPageForBackEnd dashBoardPageForBackEnd = new DashBoardPageForBackEnd(driver);
+    SalesManagerViewInvoices salesManagerViewInvoices = new SalesManagerViewInvoices(driver);
     @Given("Sales manager on the dashboard page")
     public void salesManagerOnTheDashboardPage() {
         loginPageForBackEnd.logIn(loginDataForBackEnd.getUsernameSalesManager()
                 ,loginDataForBackEnd.getPassword());
         dashBoardPageForBackEnd.verifySalesModuleDashboardOpened();
 
+    }
+
+    @Given(":Sales manager must be on the dash board page")
+    public void salesManagerMustBeOnTheDashBoardPage() {
+    }
+
+    @When(":Sales manager click on the views link for invoices")
+    public void salesManagerClickOnTheViewsLinkForInvoices() {
+        salesManagerViewInvoices.viewInvoiceAndComment();
+
+    }
+
+    @Then(":Sales manager should be able to see all invoices")
+    public void salesManagerShouldBeAbleToSeeAllInvoices() {
+        salesManagerViewInvoices.verifyInvoicesDisplay();
+    }
+
+    @When(":Sales manager comments on the invoice")
+    public void salesManagerCommentsOnTheInvoice() {
+        salesManagerViewInvoices.viewInvoiceAndComment1();
+    }
+
+    @Then(":Sales manager should be able to comment on the invoice")
+    public void salesManagerShouldBeAbleToCommentOnTheInvoice() {
+        salesManagerViewInvoices.verifyInvoicesComment();
     }
 }
