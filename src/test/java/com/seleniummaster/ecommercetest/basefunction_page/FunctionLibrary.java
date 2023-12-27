00162eefed992e4622350a74ed16d6eb4f05a1e2
tolunaybisar <tolunay.bisar@gmail.com>
2023-12-27 12:50:46 +0300

    27.12.2023 12:43 api

--- src/test/java/com/seleniummaster/ecommercetest/basefunction_page/FunctionLibrary.java
+++ src/test/java/com/seleniummaster/ecommercetest/basefunction_page/FunctionLibrary.java
@@ -141,7 +141,7 @@ public class FunctionLibrary {
 
     }
 public void waitForPresent(WebElement element){
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(180));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(300));
         wait.until(ExpectedConditions.visibilityOf(element));
 
 }
